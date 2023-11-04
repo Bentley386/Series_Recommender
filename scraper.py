b828212a2f08b361a1382bd32fcc005289416eb4
apozun <andrej.pozun@ijs.si>
2023-11-04 22:24:30 +0100

    Simplified the code and adjusted it to work with new website structure

--- scraper.py
+++ scraper.py
@@ -1,5 +1,4 @@
 import time
-import threading
 import configparser
 import re
 import storage
@@ -12,7 +11,6 @@ class Scraper():
     
     def __init__(self):
         print("Initializing")
-        self.running = False
         self.config = configparser.ConfigParser()
         self.config.read("scraper.ini")
         self.interval = int(self.config['DEFAULT']['Interval'])
@@ -21,20 +19,6 @@ class Scraper():
         self.numProcessed = int(self.config['MyAnimeList']['Processed'])
         self.storage = storage.Storage()
         
-    def start(self):
-        self.running = True
-        self.thread = threading.Thread(target=self.scrape)
-        self.thread.start()
-        print("Enter any key to terminate...")
-        input()
-        self.stop()
-        
-    def stop(self):
-        self.running = False
-        self.thread.join()
-        self.save()
-        print("Crawling stopped.")
-
     def save(self):
         self.storage.saveJSON()
         self.config['MyAnimeList']['Processed'] = str(self.numProcessed)
@@ -42,16 +26,21 @@ class Scraper():
             self.config.write(f)
         
         
-    def scrape(self):
+    def start(self):
         print("Starting to scrape")
-        while self.running:
+        while True:
             self.save()
             page = self.numProcessed // 24
-            client = urlopen(self.usersURL+str(page*24))
+            print(page)
+            try:
+                client = urlopen(self.usersURL+str(page*24))
+            except Exception as e:
+                print(self.usersURL+str(page*24))
+                raise e
             self.processUsers(soup(client.read(),"lxml"))
             client.close()
             time.sleep(self.interval)
-
+        
     def processUsers(self,page_soup):
         print("Starting to scrape on a new page")
         while True:
@@ -64,49 +53,37 @@ class Scraper():
                 break
 
     def processUser(self,user):
-        try:
-            client = urlopen(self.profileURL+user+"?status=7")
-        except urllib.error.HTTPError as e:
-            print(f"Error found while processing {user}: {e}")
-            return
+        ratings = []
+        series = []
+        page = 0
+        while True:
+            rawratings,rawnames = self.processDynamic(user,page)
+            if (len(rawratings) != len(rawnames)):
+                print(f"Something went wrong with user {user}")
+                break
+            ratings = ratings + rawratings
+            series = series + rawnames
+            if len(rawratings)<300:
+                break
+            page+=300
         
-        page_soup = soup(client.read(),"lxml")
-        info = page_soup.find_all("table",{"class":"list-table"})
-        client.close()
-        
-        rawratings = []
-        rawseries = []
-        assert len(info)<2,f"Something weird happened with {user}"
-            
-        if len(info) == 1:
-            info = info[0]["data-items"]
-            rawratings = re.findall('"score":[0-9]+,"',info)
-            rawratings = [x[8:-2] for x in rawratings]
-            rawseries = re.findall('"anime_title":.+?,"anime_num',info)
-            rawseries = [x[14:-11] for x in rawseries]
-            if len(rawseries) == 300:
-                page=300
-                more = self.processDynamic(user,page)
-                while len(more)>2:
-                    rawratings = rawratings + [x[8:-2] for x in re.findall('"score":[0-9]+,"',more)]
-                    rawseries = rawseries + [x[14:-11] for x in re.findall('"anime_title":.+?,"anime_num',more)]
-                    page+=300
-                    more = self.processDynamic(user,page)
-
-            
-        else:
-            allowed = ["-","1","2","3","4","5","6","7","8","9","0","10"]
-            rawseries = [x.span.text for x in page_soup.findAll("a",{"class":"animetitle"})]
-            rawratings = page_soup.findAll("span",{"class":"score-label"})
-            rawratings = [x.text if (x.text != '-') else '0' for x in rawratings if x.text in allowed]
-         
-        ratings = [rawratings[i] for i in range(len(rawratings)) if rawratings[i] != '0']
-        series = [rawseries[i] for i in range(len(rawratings)) if rawratings[i] != '0']
+        series = [series[i] for i in range(len(ratings)) if ratings[i] != '0']            
+        ratings = [ratings[i] for i in range(len(ratings)) if ratings[i] != '0']
         self.storage.updateObject(user,ratings,series)
     
     def processDynamic(self,user,num):
+        try:
             client = urlopen(self.profileURL+user+"/load.json?offset=" + str(num) + "&status=7")
-            return client.read().decode("utf-8")
+            resultingText = client.read().decode("utf-8")
+            time.sleep(self.interval)
+            client.close()
+            ratings = [x[8:-2] for x in re.findall('"score":[0-9]+,"',resultingText)]
+            names = [x[15:-18] for x in re.findall('"anime_title":.+?,"anime_title_eng',resultingText)]
+            return [ratings,names]
+        except urllib.error.HTTPError as e:
+            print(f"Error found while processing {user}: {e}")
+            return [[],[]]
+        
         
 if __name__ == "__main__":
     s = Scraper()
